17d5d08daf6268959d013b41328c98e1080301ef
Elian Becali <elianbecaliaguiar@gmail.com>
2021-03-22 08:04:18 -0400

    :sparkles: feat: seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Bootcamp Ignite - ReactJS',
+          type: 'withdraw',
+          category: 'Investimentos',
+          amount: 1980,
+          createdAt: new Date('2021-02-12 07:00:00')
+        },
+        {
+          id: 2,
+          title: 'Freelance website',
+          type: 'deposit',
+          category: 'desenvolvimento',
+          amount: 7000,
+          createdAt: new Date('2021-02-10 18:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
